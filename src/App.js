5cc491a0e259c99900ab3d0a968722bd65795a8a
oschlepphorst <schlepphorst.o@husky.neu.edu>
2020-03-01 14:56:29 -0500

    added main components, pushing node modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <h2 className="App-header">OLIVIA SCHLEPPHORST</h2>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav mr-auto">
+        <li><Link to={'/'} className="nav-link"> Home </Link></li>
+        <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+        <li><Link to={'/about'} className="nav-link">About</Link></li>
+      </ul>
+      </nav>
+      <hr />
+      <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/about' component={About} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
