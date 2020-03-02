6f2322e0df25e648088638d74195488caa24154f
Olivia Schlepphorst <oliviaschlepphorst@Quarantine-iMac.local>
2020-03-02 18:35:40 -0500

    fixed font sizing and spacing of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,13 @@ class Home extends Component {
           <img class="home-portrait" src={homePagePortrait} alt="home portrait"></img>
           <div class="home-header-bio">
             <h3 class="home-header">Hi! I'm Olivia.</h3>
-            <p class="home-bio">I'm currently in my fifth year at Northeastern University, pursuing a Bachelor's of Science degree in Computer Science and Mathematics.</p>
+            <p class="home-bio">I'm currently in my fifth year at Northeastern University, pursuing a Bachelor's of 
+            Science degree in Computer Science and Mathematics, and set to graduate this May 2020.</p>
+            <p class="home-bio">I am a Massachusetts native, born in the small town of Groton. I currently live in Boston
+            and absolutely love it. </p>
+            <p class="home-bio">Besides coding, I
+            enjoy photography, cooking, traveling, hanging out with friends, and being outside. Check out some of my amateur 
+            photos taken on my Nikon D5300 :)</p>
           </div>
         </div>
     );
