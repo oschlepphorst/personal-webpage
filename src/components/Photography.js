df5c1b5d0213ba59f963d4b01756d1a54ecc69e4
oschlepphorst <schlepphorst.o@husky.neu.edu>
2020-04-28 16:09:24 -0400

    added image overlay hover effects

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -14,22 +14,106 @@ class Photography extends Component {
   render() {
     return (
         <div className="photography-page">
-          {/* <p className="photography-info">Here are some favorite pictures I've taken.</p> */}
 
           <div class="photo-row1">
-            <img className="capri-img" src={capri} alt="Capri"></img>
-            <img className="athens-img" src={athens} alt="Athens"></img>
-            <img className="chairs-img" src={chairs} alt="Chairs"></img>
+
+            <div className="img-container">
+              <div class="content">
+                <div class="content-overlay"></div>
+                <img className="capri-img" src={capri} alt="Capri"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Capri, Italy</h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="athens-img" src={athens} alt="Athens"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Athens, Greece</h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="chairs-img" src={chairs} alt="Colorado"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Colorado</h3>
+                </div>
+              </div>
+            </div>
+            
           </div>
           <div class="photo-row2">
-            <img className="florence-img" src={florence} alt="Florence"></img>
-            <img className="flower-img" src={flower} alt="Flower"></img>
-            <img className="mountain-img" src={mountain} alt="Mountain"></img>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="florence-img" src={florence} alt="Florence"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Florence, Italy</h3>
+                </div>
+              </div>
             </div>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="flower-img" src={flower} alt="Flower"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Boston</h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="mountain-img" src={mountain} alt="Mountain"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">New Hampshire</h3>
+                </div>
+              </div>
+            </div>
+
+            </div>
+
           <div class="photo-row3">
-            <img className="rocks-img" src={rocks} alt="Rocks"></img>
-             <img className="santorini-img" src={santorini} alt="Santorini"></img>
-             <img className="rhode-island-img" src={rhodeIsland} alt="Rhode Island"></img>
+
+            <div className="img-container">
+                <div className="content">
+                  <div className="content-overlay"></div>
+                  <img className="rocks-img" src={rocks} alt="Rocks"></img>
+                  <div className="content-details fade-in-bottom">
+                    <h3 className="img-text">San Francisco</h3>
+                  </div>
+                </div>
+              </div>
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="santorini-img" src={santorini} alt="Santorini"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Santorini, Greece</h3>
+                </div>
+              </div>
+            </div>  
+
+            <div className="img-container">
+              <div className="content">
+                <div className="content-overlay"></div>
+                <img className="rhode-island-img" src={rhodeIsland} alt="Rhode Island"></img>
+                <div className="content-details fade-in-bottom">
+                  <h3 className="img-text">Rhode Island</h3>
+                </div>
+              </div>
+            </div>
+
           </div>
           
         </div>
